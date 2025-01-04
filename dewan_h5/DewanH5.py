f350b3e52ead2b74f96abd675b37c9713a485a07
Austin Pauley <austin2118ace@gmail.com>
2025-01-04 01:59:45 -0500

    Add imports and constructor signature

--- dewan_h5/DewanH5.py
+++ dewan_h5/DewanH5.py
@@ -4,9 +4,17 @@ Author: Austin Pauley (pauley@psy.fsu.edu)
 Date: 01-04-2025
 """
 
+import h5py
+from pathlib import Path
+from typing import Union
+
 
 class DewanH5:
-    def __init__(self):
+    def __init__(self, file_path:  Union[None, Path] = None):
+
+        self.file_path = file_path
+
+        # Parameters from H5 File
         self.mouse_number = 0
         self.total_trials = 0
         self.date = None
