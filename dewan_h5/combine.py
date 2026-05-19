465f1101890717bd3e490b1cb8ac364b1d817447
Austin Pauley <austin2118ace@gmail.com>
2026-05-19 13:48:35 -0400

    Accept and combine an arbitrary number of files

--- dewan_h5/combine.py
+++ dewan_h5/combine.py
@@ -1,71 +1,92 @@
 import argparse
-import h5py
-import numpy as np
+from datetime import datetime
 from pathlib import Path
 
-# file1 = Path(r'R:\5_Projects\1_Sniffing\3_benzaldehyde\Raw_Data\212\mouse212_sess1_D2025_9_21T15_10_39.h5')
-# file2 = Path(r'R:\5_Projects\1_Sniffing\3_benzaldehyde\Raw_Data\212\mouse212_sess1_D2025_9_21T15_36_54.h5')
-# new_file = file2.with_name(f"{file2.stem}-combined.h5")
+import h5py
+import numpy as np
 
-def combine(file1: Path, file2: Path, new_file: Path|None =None):
 
+def sort_files(*file_paths: Path) -> list[Path]:
+    dates = []
+    for file in file_paths:
+        str_date = str(file.stem).split("D")[1]
+        date = datetime.strptime(str_date, "%Y_%m_%dT%H_%M_%S")
+        dates.append(date)
+    sorted_order = np.argsort(dates)
+    return [file_paths[i] for i in sorted_order]
+
+
+def combine(*files: Path, new_file: Path | None = None):
     if new_file is None:
-        new_file = file1.with_stem(f'{file1.stem}-combined')
+        new_file = files[0].with_stem(f'{files[0].stem}-combined')
+    print(f"Output file: {new_file}")
 
-    with h5py.File(file1, 'r') as f1, h5py.File(file2, 'r')  as f2, h5py.File(new_file, 'w') as nf:
-            for attr in f1.attrs:
-                nf.attrs[attr] = f1.attrs[attr]
+    with h5py.File(files[0], 'r') as f0, h5py.File(new_file, 'w') as nf:
+        f0_trial_matrix = f0['Trials']
 
-            f1_keys = list(f1.keys())[:-1] # Ignore the 'Trials' key
-            f2_keys = list(f2.keys())[10:-1] # Ignore the first ten and the 'Trials' key
-            next_trial_num = len(f1_keys) + 1 # Trials are 1-indexed, and there is one extra key, so it works out
-            num_f2_trials = len(f2_keys)
-            new_trial_num = np.arange(next_trial_num, next_trial_num + num_f2_trials)
-            new_trial_names = ['Trial' + str(num).zfill(4) for num in new_trial_num]
-            for f1_key in f1_keys:
-                f1.copy(f1_key, nf, f1_key)
+        combined_trial_matrix = f0_trial_matrix[:]
+        # Set to f0 matrix
 
-            for f2_original, f2_new in zip(f2_keys, new_trial_names):
-                f2.copy(f2_original, nf, f2_new)
+        for attr in f0.attrs:
+            nf.attrs[attr] = f0.attrs[attr]
+        # Copy file attributes
 
-            f1_trial_matrix = f1['Trials']
-            f2_trial_matrix = f2['Trials']
+        f0_keys = list(f0.keys())[:-1] # Ignore the 'Trials' key
+        next_trial_num = len(f0_keys) + 1 # Trials are 1-indexed, and there is one extra key, so it works out
 
-            f1_trial_matrix_data = f1_trial_matrix[:]
-            f2_trial_matrix_data = f2_trial_matrix[10:]
-            nf_trial_matrix = np.concat((f1_trial_matrix_data, f2_trial_matrix_data), axis=0)
-            num_rows = len(nf_trial_matrix)
-            nf_trial_matrix_dataset = nf.create_dataset('Trials', data=nf_trial_matrix)
-            for attr in f1_trial_matrix.attrs:
-                nf_trial_matrix_dataset.attrs[attr] = f1_trial_matrix.attrs[attr]
-            nf_trial_matrix_dataset.attrs['NROWS'] = num_rows
+        for f0_key in f0_keys:
+            f0.copy(f0_key, nf, f0_key)
+        # Copy data
+
+
+        for file in files[1:]:
+            with h5py.File(file, 'r') as old_file:
+                old_file_keys = list(old_file.keys())[10:-1] # Ignore the first ten and the 'Trials' key
+
+                num_old_trials = len(old_file_keys)
+                new_trial_num = np.arange(next_trial_num, next_trial_num + num_old_trials)
+                next_trial_num = next_trial_num + num_old_trials
+                # Trials are 1-indexed, and there is one extra key, so it works out
+
+                new_trial_names = ['Trial' + str(num).zfill(4) for num in new_trial_num]
+
+                for old_original, old_new in zip(old_file_keys, new_trial_names):
+                    old_file.copy(old_original, nf, old_new)
+
+                old_file_trial_matrix_data = old_file['Trials'][10:]
+                # Skip the first ten
+                combined_trial_matrix = np.concat((combined_trial_matrix, old_file_trial_matrix_data), axis=0)
+
+        num_rows = len(combined_trial_matrix)
+        nf_trial_matrix_dataset = nf.create_dataset('Trials', data=combined_trial_matrix)
+        for attr in f0_trial_matrix.attrs:
+            nf_trial_matrix_dataset.attrs[attr] = f0_trial_matrix.attrs[attr]
+        nf_trial_matrix_dataset.attrs['NROWS'] = num_rows
+
 
 def main():
     parser = argparse.ArgumentParser(description='Combine two HDF5 files')
-    parser.add_argument('file1', help='First HDF5 file')
-    parser.add_argument('file2', help='Second HDF5 file')
-    parser.add_argument('-o', '--output', help='New HDF5 file')
+    parser.add_argument('files', nargs="+", action="extend", help='HDF Files')
+    parser.add_argument('-o', '--output', default=None, help='New HDF5 file')
 
     args = parser.parse_args()
 
-    file1_path = Path(args.file1)
-    file2_path = Path(args.file2)
+    file_paths = [Path(file) for file in args.files]
 
-    if not file1_path.exists():
-        raise FileNotFoundError(f'File 1 [{args.file1}] does not exist')
-    if not file2_path.exists():
-        raise FileNotFoundError(f'File 2 [{args.file2}] does not exist')
+    for file in file_paths:
+        if not file.exists():
+            raise FileNotFoundError(f'File 1 [{args.file1}] does not exist')
 
-    kwargs = {
-        'file1': file1_path,
-        'file2': file2_path,
-    }
+
+    kwargs = {}
 
     if args.output:
         output_path = Path(args.output)
+        kwargs['output_file'] = output_path
         if not output_path.parent.exists():
             raise FileNotFoundError(f'Output directory {args.output} does not exist')
 
-        kwargs['new_file'] = output_path
+    combine(*file_paths, **kwargs)
 
-    combine(**kwargs)
+if __name__ == '__main__':
+    main()
